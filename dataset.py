0ee834599d9dc7bae730ee044154aa9e00e33c20
夏叶子 <xiayezi@xiayezideMacBook-Air.local>
2019-10-11 14:19:41 +0800

    Initial beta-vae code

--- dataset.py
+++ dataset.py
@@ -3,10 +3,22 @@
 Created on Wed Oct  9 18:42:00 2019
 
 @author: Joshua
+
+The dsprite-dataset, https://github.com/deepmind/dsprites-dataset
+
+It has (737280 x 64 x 64, uint8) images in black and white, under key 'imgs'
+It has (737280 x 6, float64) values of the latent factors, under key 'latents_values'
+    1st: 1          Color, always 1.00
+    2nd: 1,2,3      Shape, square, ellipse, heart
+    3rd: 0.5~0.1    Scale, 6 values linearly between 0.5 and 1.0
+    4th: 0.0~2pi    Rotation, 40 values linearly between 0 and 2pi
+    5th: 0.0~1.0    Position X, 32 values between 0 and 1
+    6th: 0.0~1.0    Position Y, 32 values between 0 and 1
 """
 
 import os
 import numpy as np
+from matplotlib import pyplot as plt
 
 import torch
 import torch.utils.data as Data # Dataset, DataLoader
@@ -18,6 +30,10 @@ def is_power_of_2(num):
 
 
 def return_data(args):
+    '''
+        The initial version of data provider, no conditional sampling implemented
+        Only feed all 737280 imgs and values to the data iterater.
+    '''
     dset_dir = args.dset_dir
     batch_size = args.batch_size
     num_workers = args.num_workers
@@ -26,11 +42,12 @@ def return_data(args):
 
     root = os.path.join(dset_dir, 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
     data = np.load(root, allow_pickle=True, encoding='bytes')
-    imgs = torch.from_numpy(data['imgs']).unsqueeze(1)
-# =================
+    imgs = torch.from_numpy(data['imgs']).unsqueeze(1).float()
+    vals = torch.from_numpy(data['latents_values']).unsqueeze(1)
+    data_set = Data.TensorDataset(imgs,vals)
 
     train_loader = Data.DataLoader(
-                              dataset = imgs,
+                              dataset = data_set,
                               batch_size=batch_size,
                               shuffle=True,
                               num_workers=num_workers,
@@ -41,10 +58,70 @@ def return_data(args):
 
     return data_loader
 
-test =return_data(args)
-flag = 0
-for x in test:
-    print(x)
-    flag += 1
-    if flag >2:
-        break
+"""
+# =========== Test for sampling data with specific latent values ==============
+root = os.path.join(args.dset_dir, 'dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
+dataset_zip = np.load(root, allow_pickle=True, encoding='bytes')
+
+print('Keys in the dataset:', dataset_zip.keys())
+imgs = dataset_zip['imgs']
+latents_values = dataset_zip['latents_values']
+latents_classes = dataset_zip['latents_classes']
+metadata = dataset_zip['metadata'][()]
+latents_sizes = metadata[b'latents_sizes']
+latents_bases = np.concatenate((latents_sizes[::-1].cumprod()[::-1][1:],np.array([1,])))
+
+
+# Helper function to show images
+def show_images_grid(imgs_, num_images=25):
+  ncols = int(np.ceil(num_images**0.5))
+  nrows = int(np.ceil(num_images / ncols))
+  _, axes = plt.subplots(ncols, nrows, figsize=(nrows * 3, ncols * 3))
+  axes = axes.flatten()
+
+  for ax_i, ax in enumerate(axes):
+    if ax_i < num_images:
+      ax.imshow(imgs_[ax_i], cmap='Greys_r',  interpolation='nearest')
+      ax.set_xticks([])
+      ax.set_yticks([])
+    else:
+      ax.axis('off')
+
+def show_density(imgs):
+  _, ax = plt.subplots()
+  ax.imshow(imgs.mean(axis=0), interpolation='nearest', cmap='Greys_r')
+  ax.grid('off')
+  ax.set_xticks([])
+  ax.set_yticks([])
+
+def latent_to_index(latents):
+  return np.dot(latents, latents_bases).astype(int)
+
+
+def sample_latent(size=1):
+  samples = np.zeros((size, latents_sizes.size))
+  for lat_i, lat_size in enumerate(latents_sizes):
+    samples[:, lat_i] = np.random.randint(lat_size, size=size)
+
+  return samples
+
+# ============ Conditional Sampling Example ===================================
+## Fix posX latent to left
+latents_sampled = sample_latent(size=10)
+latents_sampled[:, -2] = 0
+indices_sampled = latent_to_index(latents_sampled)
+imgs_sampled = imgs[indices_sampled]
+# Samples
+show_images_grid(imgs_sampled, 9)
+
+# Show the density too to check
+show_density(imgs_sampled)
+"""
+if __name__ == '__main__':
+    test =return_data(args)
+    flag = 0
+    for x,y in test:
+        print(x,y)
+        flag += 1
+        if flag >2:
+            break
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import argparse
 import numpy as np
 import torch
 
-#from solver import Solver
+from solver import Solver
 from utils import str2bool
 
 torch.backends.cudnn.enabled = True
@@ -25,14 +25,8 @@ def main(args):
     torch.cuda.manual_seed(seed)
     np.random.seed(seed)
 
-'''
     net = Solver(args)
-
-    if args.train:
-        net.train()
-    else:
-        net.traverse()
-'''
+    net.train()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='toy Beta-VAE')
@@ -40,16 +34,16 @@ if __name__ == "__main__":
     parser.add_argument('--train', default=True, type=str2bool, help='train or traverse')
     parser.add_argument('--seed', default=1, type=int, help='random seed')
     parser.add_argument('--cuda', default=True, type=str2bool, help='enable cuda')
-    parser.add_argument('--max_iter', default=1e6, type=float, help='maximum training iteration')
+    parser.add_argument('--max_iter', default=1e3, type=float, help='maximum training iteration')
     parser.add_argument('--batch_size', default=64, type=int, help='batch size')
 
     parser.add_argument('--z_dim', default=10, type=int, help='dimension of the representation z')
     parser.add_argument('--beta', default=4, type=float, help='beta parameter for KL-term in original beta-VAE')
-    parser.add_argument('--objective', default='H', type=str, help='beta-vae objective proposed in Higgins et al. or Burgess et al. H/B')
-    parser.add_argument('--model', default='H', type=str, help='model proposed in Higgins et al. or Burgess et al. H/B')
-    parser.add_argument('--gamma', default=1000, type=float, help='gamma parameter for KL-term in understanding beta-VAE')
-    parser.add_argument('--C_max', default=25, type=float, help='capacity parameter(C) of bottleneck channel')
-    parser.add_argument('--C_stop_iter', default=1e5, type=float, help='when to stop increasing the capacity')
+    #parser.add_argument('--objective', default='H', type=str, help='beta-vae objective proposed in Higgins et al. or Burgess et al. H/B')
+    #parser.add_argument('--model', default='H', type=str, help='model proposed in Higgins et al. or Burgess et al. H/B')
+    #parser.add_argument('--gamma', default=1000, type=float, help='gamma parameter for KL-term in understanding beta-VAE')
+    #parser.add_argument('--C_max', default=25, type=float, help='capacity parameter(C) of bottleneck channel')
+    #parser.add_argument('--C_stop_iter', default=1e5, type=float, help='when to stop increasing the capacity')
     parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')
     parser.add_argument('--beta1', default=0.9, type=float, help='Adam optimizer beta1')
     parser.add_argument('--beta2', default=0.999, type=float, help='Adam optimizer beta2')
@@ -64,7 +58,8 @@ if __name__ == "__main__":
 
     parser.add_argument('--ckpt_dir', default='checkpoints', type=str, help='checkpoint directory')
     parser.add_argument('--ckpt_name', default='last', type=str, help='load previous checkpoint. insert checkpoint filename')
-
+    parser.add_argument('--exp_name', default='test', type=str, help='name of the experiment')
+    
     args = parser.parse_args()
 
     main(args)
